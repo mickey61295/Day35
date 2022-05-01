1779a1f7dc1f7cbbb3f5e66b3191275f5fe6ed0f
Maheshbabu Jeyaraj <j.maihby@gmail.com>
2022-05-01 10:03:22 +0530

    Done

--- index.js
+++ index.js
@@ -28,38 +28,75 @@ app.get('/', (req, res) => {
 })
 
 app.post('/create-mentor', async (req, res) => {
-	const { name, email } = req.body
-	const mentorCount = await client
-		.db('day35')
-		.collection('mentors')
-		.countDocuments()
-	const mentorId = mentorCount ? mentorCount + 1 : 1
-
-	const mentor = await client.db('day35').collection('mentors').insertOne({
-		mentor_id: mentorId,
-		name: name,
-		email: email,
-		students: [],
-	})
-	res.send(mentor)
+	if (req.body.length) {
+		const mentors = req.body
+		const mentorCount = await client
+			.db('day35')
+			.collection('mentors')
+			.countDocuments()
+		var mentorId = mentorCount ? mentorCount + 1 : 1
+		const mentor = await client
+			.db('day35')
+			.collection('mentors')
+			.insertMany(
+				mentors.map((item) => {
+					return {
+						mentor_id: mentorId++,
+						name: item.name,
+						email: item.email,
+						students: [],
+					}
+				})
+			)
+		res.send(mentor)
+	} else {
+		const mentor = await client
+			.db('day35')
+			.collection('mentors')
+			.insertOne(req.body)
+		res.send(mentor)
+	}
 })
 
 app.post('/create-student', async (req, res) => {
-	const { name, email, batch } = req.body
-	const studentCount = await client
-		.db('day35')
-		.collection('students')
-		.countDocuments()
-	const studentId = studentCount ? studentCount + 1 : 1
+	if (req.body.length) {
+		const students = req.body
+		const studentCount = await client
+			.db('day35')
+			.collection('students')
+			.countDocuments()
+		var studentId = studentCount ? studentCount + 1 : 1
+		const student = await client
+			.db('day35')
+			.collection('students')
+			.insertMany(
+				students.map((item) => {
+					return {
+						student_id: studentId++,
+						name: item.name,
+						email: item.email,
+						mentor_id: null,
+					}
+				})
+			)
+		res.send(student)
+	} else {
+		const { name, email, batch } = req.body
+		const studentCount = await client
+			.db('day35')
+			.collection('students')
+			.countDocuments()
+		const studentId = studentCount ? studentCount + 1 : 1
 
-	const student = await client.db('day35').collection('students').insertOne({
-		student_id: studentId,
-		name: name,
-		email: email,
-		batch: batch,
-		mentor: null,
-	})
-	res.send(student)
+		const student = await client.db('day35').collection('students').insertOne({
+			student_id: studentId,
+			name: name,
+			email: email,
+			batch: batch,
+			mentor: null,
+		})
+		res.send(student)
+	}
 })
 
 app.get('/mentors', async (req, res) => {
@@ -82,70 +119,40 @@ app.get('/students', async (req, res) => {
 
 app.put('/assign-student', async (req, res) => {
 	const { studentId, mentorId } = req.body
-	const students = await client
+	const studentswithNoMentor = await client
 		.db('day35')
 		.collection('students')
-		.find({ mentor: null })
+		.find({ mentor_id: null, student_id: { $in: studentId } })
 		.toArray()
 
-	const filteredStudents = students.filter((student) => {
-		return student.student_id === studentId
+	let mentorstudentArray = (
+		await client
+			.db('day35')
+			.collection('mentors')
+			.find({ mentor_id: mentorId }, { students: 1, _id: 0 })
+			.toArray()
+	)[0].students
+
+	studentId.forEach((item) => {
+		mentorstudentArray.push(item)
 	})
 
-	if (studentId.isArray) {
-		const mentor = await client
-			.db('day35')
-			.collection('mentors')
-			.updateOne(
-				{ mentor_id: mentorId },
-				{ $push: { students: { $each: studentId } } }
-			)
-		res.send(mentor)
-		studentId.forEach(async (id) => {
-			await client
-				.db('day35')
-				.collection('students')
-				.updateOne(
-					{
-						student_id: id,
-					},
-					{
-						$set: {
-							mentor: mentorId,
-						},
-					}
-				)
-		})
-	} else {
-		await client
-			.db('day35')
-			.collection('mentors')
-			.updateOne(
-				{
-					mentor_id: mentorId,
-				},
-				{
-					$push: {
-						students: studentId,
-					},
-				}
-			)
-
-		await client
-			.db('day35')
-			.collection('students')
-			.updateOne(
-				{
-					student_id: studentId,
-				},
-				{
-					$set: {
-						mentor: mentorId,
-					},
-				}
-			)
-	}
-	res.send('Student assigned to mentor')
+	console.log(mentorstudentArray)
+	const student = await client
+		.db('day35')
+		.collection('students')
+		.updateMany(
+			{ student_id: { $in: studentId } },
+			{ $set: { mentor_id: mentorId } }
+		)
+	const mentor = await client
+		.db('day35')
+		.collection('mentors')
+		.updateOne(
+			{ mentor_id: mentorId },
+			{ $set: { students: mentorstudentArray } }
+		)
+	res.send({ student, mentor })
 })
 
 app.put('/assign-mentor', async (req, res) => {
@@ -172,7 +179,7 @@ app.get('/mentor/:mentorId', async (req, res) => {
 	const students = await client
 		.db('day35')
 		.collection('students')
-		.find({ mentor: parseInt(mentorId) }, { projection: { name: 1 } })
+		.find({ mentor: parseInt(mentorId) }, { projection: { name: 1, _id: 0 } })
 		.toArray()
 	res.send(students)
 })
